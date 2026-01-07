6504169f3e5c39103bc5f066c2a1f3c8adc903c3
tanushabhinavshah <tanush.shah@joshsoftware.com>
2026-01-07 22:52:17 +0530

    Added comments to improve readability

--- submissions/reverse-a-string_tanush.go
+++ submissions/reverse-a-string_tanush.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func reverseString(s string) string {
-	runes := []rune(s)
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]
+	runes := []rune(s)					// runes are used in order to handle special characters like emojis correctly
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {	
+		runes[i], runes[j] = runes[j], runes[i]		// swap the characters at position i and j
 	}
-	return string(runes)
+	return string(runes)					// converts the rune back to string type and returns
 }
 
 func main() {
