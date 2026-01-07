3c7d6978f975b4bf3da3f84c51d22e3f34407c2b
tanushabhinavshah <tanush.shah@joshsoftware.com>
2026-01-07 20:34:04 +0530

    removed palindrome_tanush.rb file (mistake)

--- submissions/palindrome_tanush.rb
+++
@@ -1,8 +0,0 @@
-def palindrome?(str)
-  str = str.downcase.gsub(/\W/, '')  # remove non-alphanumeric chars
-  str == str.reverse
-end
-
-puts palindrome?("Racecar")   # true
-puts palindrome?("Hello")     # false
-
