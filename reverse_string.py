a9f39f0832a34d8ebbbf1853455e5b215ef106d7
sujaljunghare-josh <sujal.junghare@joshsoftware.com>
2026-01-08 20:39:47 +0530

    Update reverse_string.py

--- reverse_string.py
+++ reverse_string.py
@@ -1,5 +1,5 @@
 def reverse_string(s):
-    return s[::-1]
+    return s[::-1] #changed step to -1 for reverse iteration on string slicing
 
 text = "Ubuntu Git Training"
 print(f"Original: {text}")
