db25ed8649ec6090ea436d7482d4d2754b2eff49
visheshchandok0605 <vishesh.chandok@joshsoftware.com>
2026-01-08 20:31:46 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -2,4 +2,4 @@ def factorial(n):
     return 1 if (n==1 or n==0) else n * factorial(n-1)
 
 num = 5
-print(f"Factorial of {num} is {factorial(num)}")
+print("Factorial of", num, "is", factorial(num))
