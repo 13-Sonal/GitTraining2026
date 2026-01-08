7ee15532a4f42f019b0d1bbdc4df4f49c7bc45ad
lukareh <harish.lukare@joshsoftware.com>
2026-01-08 22:04:18 +0530

    added input validation and use long long to prevent overflow

--- submissions/harish/fibonacci.cpp
+++ submissions/harish/fibonacci.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 using namespace std;
 
-#Fibonacci series cpp program
+// Fibonacci series cpp program
 int main() {
     int n;
     cin >> n;
 
-    int a = 0, b = 1;
+    if (n < 0) {
+        cout << "Invalid input. Please enter a non-negative number." << endl;
+        return 0;
+    }
+
+    long long a = 0, b = 1;
 
     for (int i = 1; i <= n; i++) {
         cout << a << " ";
-        int next = a + b;
+        long long next = a + b;
         a = b;
         b = next;
     }
