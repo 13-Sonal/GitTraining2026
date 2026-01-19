3cafba689a4612ad92e40de9de1662856ff32999
Shubhangi1qa <shubhangi.durbe@joshsoftware.com>
2026-01-19 16:32:24 +0530

    reverse string program added

---
+++ reverse_string.java
@@ -0,0 +1,14 @@
+# reverse string program java
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String str = "hello";
+        String rev = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev += str.charAt(i);
+        }
+
+        System.out.println(rev);
+    }
+}
